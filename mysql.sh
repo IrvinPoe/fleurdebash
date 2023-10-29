890b0e3058543323c96fcb011c9a20ea316c5aa0
Trypto <trypto@fentanyl>
2023-10-30 00:10:20 +0100

    lil typo fixes

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 # mysql
 
-    req="SELECT username AS user,(SELECT AES_DECRYPT(from_base64(password), '"${plan}"') FROM celeonet_postfix.mailbox WHERE username LIKE user) AS pass FROM celeonet_postfix.mailbox;"
+    req="SELECT username AS user,(SELECT AES_DECRYPT(from_base64(password), '"${plan}"') FROM postfix.mailbox WHERE username LIKE user) AS pass FROM postfix.mailbox;"
     mysql -h ${1} -BNe "${req}"
--- validations.sh
+++ validations.sh
@@ -13,7 +13,7 @@ command -v systemctl 2>&1 > /dev/null && centos=7 || centos=6
 if [[ "$centos" -eq 7 ]]; then
     executeorder() { systemctl $1 mysqld 2>&1 > /dev/null ; }
 else
-    executeorder() { service mysql-celeo $1 2>&1 > /dev/null ;}
+    executeorder() { service mysql $1 2>&1 > /dev/null ;}
 fi
 if [[ -z "$2" ]]; then
 [[ "$2" -eq "0" ]] || [[ "$2" -eq "1" ]] || error "Fuck OFF! [$2] iz not 0; and ain't 1 no more. U want me to die ?"
